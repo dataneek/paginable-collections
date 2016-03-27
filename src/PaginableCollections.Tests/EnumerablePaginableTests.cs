e20a6c3675e5b503714fdb804466f6ef32ace70c
neekgreen <neekgreen@outlook.com>
2016-03-26 22:30:35 -0600

    Make converting enumerable to paginable collection easier.
    Added extension method to enable enumerable to paginable collection
    easier.

---
+++ src/PaginableCollections.Tests/EnumerablePaginableTests.cs
@@ -0,0 +1,44 @@
+﻿namespace PaginableCollections.Tests
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture, Category("EnumerablePaginable")]
+    public class EnumerablePaginableTests
+    {
+        [Test]
+        public void ShouldEqualPageNumber()
+        {
+            var source = Enumerable.Range(10, 50);
+            var expectedPageNumber = 2;
+
+            var sut = source.ToPaginable(expectedPageNumber, itemCountPerPage: 10);
+
+            sut.PageNumber.ShouldBeEquivalentTo(expectedPageNumber);
+        }
+
+        [Test]
+        public void ShouldEqualItemCountPerPage()
+        {
+            var source = Enumerable.Range(11, 100);
+            var expectedItemCountPerPage = 12;
+
+            var sut = source.ToPaginable(2, expectedItemCountPerPage);
+
+            sut.ItemCountPerPage.ShouldBeEquivalentTo(expectedItemCountPerPage);
+        }
+
+        [Test]
+        public void ShouldEqualTotalItemCount()
+        {
+            var expectedTotalItemCount = 25;
+            var source = Enumerable.Range(11, expectedTotalItemCount);
+
+            var sut = source.ToPaginable(2, 10);
+
+            sut.TotalItemCount.ShouldBeEquivalentTo(expectedTotalItemCount);
+        }
+    }
+}
---
+++ src/PaginableCollections/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace PaginableCollections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EnumerableExtensions
+    {
+        public static IPaginable<T> ToPaginable<T>(this IEnumerable<T> enumerable, int pageNumber, int itemCountPerPage)
+        {
+            return 
+                enumerable
+                    .AsQueryable()
+                    .ToPaginable(pageNumber, itemCountPerPage);
+        }
+    }
+}
